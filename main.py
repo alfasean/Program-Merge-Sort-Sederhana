84c3a95283350599674847892fad0bed80a828c0
alfasean <alfasean22@gmail.com>
2021-06-08 22:39:28 +0800

    first commit

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def merge(left, right):
     result = []
     while left_index < len(left) and right_index < len(right): 
         if left[left_index] < right[right_index]:
-            result.append(right[right_index])
-            right_index += 1
-        else:
             result.append(left[left_index])
             left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
 
     result += left[left_index:]
     result += right[right_index:]
